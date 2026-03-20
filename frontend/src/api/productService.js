f529b914459bbe6abf6d26ba7ebc20932ebb2f23
aadity-a <aadi042105@gmail.com>
2026-03-20 17:18:30 +0530

    code fix

--- frontend/src/api/productService.js
+++ frontend/src/api/productService.js
@@ -1,10 +1,10 @@
 export const fetchProducts = async (filters) => {
-    let url = new URL('http://localhost:8080/api/products');
+    let url = new URL('https://productstore-production-30e0.up.railway.app/api/products');
     if (filters.category) url.searchParams.append('category', filters.category);
     if (filters.minPrice) url.searchParams.append('minPrice', filters.minPrice);
     if (filters.maxPrice) url.searchParams.append('maxPrice', filters.maxPrice);
     if (filters.sort) url.searchParams.append('sortRule', filters.sort);
-    
+
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error('Failed to fetch products');
